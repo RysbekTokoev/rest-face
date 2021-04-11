7b3241b5751dbc6b098788186a1eaa2efaddb47d
RysbekTokoev <45264163+RysbekTokoev@users.noreply.github.com>
2021-04-11 04:29:55 -0700

    secret_key updated

--- config/config/settings.py
+++ config/config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
-if not SECRET_KEY:
-    SECRET_KEY = get_random_secret_key()
+SECRET_KEY = '*$gxa(xw=_okf3tc7w78$#rer!5q_)-srrd8b$6-7*@fvk95cx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
