6d31001e1a08da748a2f10f6bb73404bd9d28eed
rtokoev <rtokoev@devcats.kg>
2024-05-08 11:15:57 +0600

    CORS save

--- config/config/settings.py
+++ config/config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'main',
     'portal',
 
+    'django_filters',
+    'corsheaders',
     'djoser',
     'rest_framework',
     'rest_framework.authtoken',
@@ -49,11 +51,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 ROOT_URLCONF = 'config.urls'
