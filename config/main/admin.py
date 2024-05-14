61b931f1fabe5adb8271f59e0a569be7f10ee6c8
rtokoev <rtokoev@devcats.kg>
2024-05-14 15:50:38 +0600

    better recognitions admin view

--- config/main/admin.py
+++ config/main/admin.py
@@ -4,4 +4,4 @@ from .models import Face, Emotion, Recognition
 
 admin.site.register(Face)
 admin.site.register(Emotion)
-admin.site.register(Recognition)
+admin.site.register(Recognition, list_display=[field.name for field in Recognition._meta.get_fields()])
