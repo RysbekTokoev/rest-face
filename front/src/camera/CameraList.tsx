24cae487d0504513c824342fa79b758a458f4159
rtokoev <rtokoev@devcats.kg>
2024-05-14 16:55:28 +0600

    camera list update

--- front/src/camera/CameraList.tsx
+++ front/src/camera/CameraList.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import './App.scss';
 import PageTemplate from "../common/PageTemplate";
 import Title from "../dashboard/Title";
 import Table from "@mui/material/Table";
@@ -8,6 +7,10 @@ import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
 import TableBody from "@mui/material/TableBody";
 import axios from "axios";
+import {TablePagination} from "@mui/material";
+import Box from "@mui/system/Box";
+import Paper from "@mui/material/Paper";
+import { Link as RouterLink } from 'react-router-dom';
 
 
 export interface ICamera {
@@ -21,52 +24,89 @@ interface ICameraList {
     next: string;
     previous: string;
     results: ICamera[];
-  }
+}
+
+interface Page {
+  count: number;
+  next: string | null;
+  previous: string | null;
+}
 
 const defaultCamera:ICamera[] = [];
 
-
 const CameraList = () => {
     const [cameras, setCameras]: [ICamera[], (cameras: ICamera[]) => void] = React.useState(defaultCamera);
-    const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
-    const [error, setError]: [string, (error: string) => void] = React.useState("");
+    const [page, setPage]: [Page, (page: Page) => void] = React.useState<Page>({count: 0, next: null, previous: null });
+    const [currentPage, setCurrentPage] = React.useState(0);
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setCurrentPage(newPage);
+    };
 
     React.useEffect(() => {
         axios
-            .get<ICameraList>("http://127.0.0.1:8000/api/portal/camera/", {
+            .get<ICameraList>(`http://127.0.0.1:8000/api/portal/camera/?page=${currentPage + 1}`, {
               headers: {
                 "Content-Type": "application/json"
               },
             }).then(response => {
-                console.log(response.data);
-              setCameras(response.data.results);
-              setLoading(false);
+                setCameras(response.data.results);
+                setPage(response.data);
             });
-    }, []);
+    }, [currentPage]);
 
-	return (
-		<PageTemplate>
-    <React.Fragment>
-      <Title>Камеры</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell>Название</TableCell>
-            <TableCell align="right">Статус</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cameras.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell align="right">{row.status}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </React.Fragment>
-  </PageTemplate>
-	);
-}; export default CameraList;
+    return (
+        <PageTemplate>
+            <Box maxWidth="75%" margin="auto">
+                <Paper elevation={3}>
+                    <Box mt={4} mb={4} mx={2}>
+                        <Box display="flex" justifyContent="space-between" alignItems="center">
+                            <Title>Камеры</Title>
+                            <TablePagination
+                                component="div"
+                                count={page.count}
+                                page={currentPage}
+                                onPageChange={handleChangePage}
+                                rowsPerPage={20}
+                                rowsPerPageOptions={[20]}
+                            />
+                        </Box>
+                        <Table size="small">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>id</TableCell>
+                                    <TableCell>Название</TableCell>
+                                    <TableCell align="right">Статус</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {cameras.map((row) => (
+                                    <TableRow key={row.id}>
+                                        <TableCell>{row.id}</TableCell>
+                                        <TableCell>
+                                            <RouterLink to={`/camera/${row.id}/`}>
+                                              {row.name}
+                                            </RouterLink>
+                                        </TableCell>
+                                        <TableCell align="right">{row.status}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                        <TablePagination
+                            component="div"
+                            count={page.count}
+                            page={currentPage}
+                            onPageChange={handleChangePage}
+                            rowsPerPage={20}
+                            rowsPerPageOptions={[20]}
+                        />
+                    </Box>
+                </Paper>
+            </Box>
+
+        </PageTemplate>
+    );
+};
+
+export default CameraList;
